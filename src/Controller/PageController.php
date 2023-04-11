252a8d0b043c35ec1715b469e9ca4503b41d4514
Evan Moreau <moreeva@ndlp.fr>
2023-04-11 17:10:19 +0200

    PatchV2

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -5,10 +5,11 @@ namespace App\Controller;
 use App\Entity\Page;
 use App\Form\PageType;
 use App\Repository\PageRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/page')]
 class PageController extends AbstractController
@@ -75,14 +76,16 @@ class PageController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_page_delete', methods: ['POST'])]
-    public function delete(Request $request, Page $page, PageRepository $pageRepository): Response
+    public function delete(Request $request, Page $page, PageRepository $pageRepository, TranslatorInterface $translator): Response
     {
         $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN');
         
         if ($this->isCsrfTokenValid('delete'.$page->getId(), $request->request->get('_token'))) {
+            if ($page->getId() < 11) {
+                $this->addFlash('Warning', $translator->trans('This page is protected !'));
+            }
             $pageRepository->remove($page, true);
         }
-
         return $this->redirectToRoute('app_page_index', [], Response::HTTP_SEE_OTHER);
     }
 }
